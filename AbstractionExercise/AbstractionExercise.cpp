d2550bfaecb492f6d1f86737fbcb01226624a801
270115050 <79881479+270115050@users.noreply.github.com>
2021-08-20 15:31:00 +1200

    Update AbstractionExercise.cpp

--- AbstractionExercise/AbstractionExercise.cpp
+++ AbstractionExercise/AbstractionExercise.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 
@@ -7,87 +9,130 @@ using namespace std;
 //the functionality of the other product they only need to worry how they are going to implement the provided funciton.
 //Any derived class that inherits the Laptop will need to provide there own implementation of all virtual funciton if not it will give syntax error.
 class Laptop {
-public:
+public:  
     //This mean that anyone who inherits Laptop will need to provide there own implementation of this function
     virtual void husbandoSpecs() = 0;
     virtual void husbandoPrice() = 0;
+
 };
 
-class Dell:public Laptop {
+class Dell:public Laptop{
 public:
     void husbandoSpecs() {
-        cout << "\nAikuro Mikisugi <3\n";
+        cout << "Aikuro Mikisugi <3";
     }
-    virtual void husbandoPrice() {
-        cout << "\n$69.00 <3\n";
+    void husbandoPrice() {
+        cout << "$69.00 <3";
     }
 };
 
 class Hp:public Laptop {
 public:
     void husbandoSpecs() {
-        cout << "\nLevi Ackerman\n";
+        cout << "Levi Ackerman";
     }
-    virtual void husbandoPrice() {
-        cout << "\n$3003.00 <3\n";
-    }
-};
-
-class Shapes {
-protected:
-    double base;
-    double height;
-
-public:
-    virtual void getData() = 0;
-    virtual void displayArea() = 0;
-};
-
-class Rectangle : public Shapes {
-public:
-    void displayArea() {
-        cout << "\nThe area is: " << base * height << endl;
-    }
-
-    void getData() {
-        cout << "\nPlease enter the Length in centimeters: ";
-        cin >> base;
-        cout << "\nPlease enter the Width in centimeters: ";
-        cin >> height;
+    void husbandoPrice() {
+        cout << "$3003.00 <3";
     }
 };
 
-class Triangle : public Shapes {
-public:
-    void displayArea() {
-        cout << "\nThe area is: " << base * height / 2 << endl;
-    }
-    void getData() {
-        cout << "\nPlease enter the base in centimeters: ";
-        cin >> base;
-        cout << "\nPlease enter the height in centimeters: ";
-        cin >> height;
-    }
-};
+//template <class Derived>
+//class Base {
+//public: 
+//    void interface() {
+//        cout << "Base::interface called\n";
+//        static_cast<Derived*>(this)->implementation();
+//    }
+//};
+
+//class Derived :public Base<Derived> {
+//public:
+//    void implementation() {
+//        cout << "hello my worthless life\n";
+//    }
+//};
+
+//class Shapes {
+//protected:
+//    double base;
+//    double height;
+//
+//public:
+//    virtual void getData() = 0;
+//    virtual void displayArea() = 0;
+//};
+//
+//class Rectangle : public Shapes {
+//public:
+//    void displayArea() {
+//        cout << "\nThe area is: " << base * height << endl;
+//    }
+//
+//    void getData() {
+//        cout << "\nPlease enter the Length in centimeters: ";
+//        cin >> base;
+//        cout << "\nPlease enter the Width in centimeters: ";
+//        cin >> height;
+//    }
+//};
+//
+//class Triangle : public Shapes {
+//public:
+//    void displayArea() {
+//        cout << "\nThe area is: " << base * height / 2 << endl;
+//    }
+//    void getData() {
+//        cout << "\nPlease enter the base in centimeters: ";
+//        cin >> base;
+//        cout << "\nPlease enter the height in centimeters: ";
+//        cin >> height;
+//    }
+//};
 
 int main()
 {
-    Laptop* l1 = new Dell();
-    Laptop* l2 = new Hp();
-    l1->husbandoSpecs();
-    l1->husbandoPrice();
-    l2->husbandoSpecs();
-    l2->husbandoPrice();
+    vector<Laptop*>l;
+    int choice,num;
+    bool isTrue;
 
-    Shapes* s1 = new Rectangle();
-    Shapes* s2 = new Triangle();
+    do {
+        isTrue = true, num = 0, choice = 0;
+        cout << "\nPick Brand"
+            << "\nPress 1 for Dell"
+            << "\nPress 2 for HP"
+            << "\nPress 3 to exit..."
+            << "\nChoose options: ";
+        cin >> choice;
 
-    cout << "\nArea of Rectangle\n";
-    s1->getData();
-    s1->displayArea();
-    cout << "\nArea of Triangle\n";
-    s2->getData();
-    s2->displayArea();
+        switch (choice) {
+        case 1: 
+            cout << "How many Laptop would you like to create?: ";
+            cin >> num;
+            for (int i = 0; i < num; i++)
+                l.push_back(new Dell);
+            break;
+        case 2:
+            cout << "How many Laptop would you like to create?: ";
+            cin >> num;
+            for (int i = 0; i < num; i++)
+                l.push_back(new Hp);
+            break;
+        case 3:
+            isTrue = false;
+            break;
+        default:
+            cout << "\nInvalid Input, Please try again...\n";
+            break;
+        }
+    } while (isTrue);
 
+    cout << "\n\n\n";
+    for (auto item : l) {
+        cout << "My husbando is "; 
+        item->husbandoSpecs(); 
+        cout << " the price I payed ";
+        item->husbandoPrice();
+        cout << endl << endl;
+    }
     return 0;
 }
